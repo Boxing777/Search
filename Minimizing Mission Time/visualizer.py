4ebaf4fff7b5b5093c73986407cc7021d07f12d4
Boxing777 <ncku.boxing123@gmail.com>
2026-03-26 15:09:50 +0800

    0

--- Minimizing Mission Time/visualizer.py
+++ Minimizing Mission Time/visualizer.py
@@ -137,10 +137,10 @@ def plot_final_comparison_trajectories(gns: np.ndarray, data_center_pos: Tuple[f
             #             zorder=5)
             #     eo_label_added = True
 
-    '''
+    """
     for i, (uav_id, segments) in enumerate(bob_trajectories.items()):
         color = 'cyan' 
-        ax.plot([], [], color=color, linestyle='-.', linewidth=2.0, label=f'{uav_id} BOB-V') # Label �אּ BOB-V
+        ax.plot([], [], color=color, linestyle='-.', linewidth=2.0, label=f'{uav_id} BOB-V') # Label �אּ BOB-V
 
         if not segments: continue
         previous_pos = data_center_pos
@@ -152,7 +152,7 @@ def plot_final_comparison_trajectories(gns: np.ndarray, data_center_pos: Tuple[f
             previous_pos = fop 
         
         ax.plot([previous_pos[0], data_center_pos[0]], [previous_pos[1], data_center_pos[1]], color=color, linestyle='-.', linewidth=1.5, zorder=2)
-    '''
+    """
     
     for i, (uav_id, segments) in enumerate(bob_f_trajectories.items()):
         color = 'magenta' 
